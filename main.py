a641106f82dc7d7b0984edec77a680defb05a013
Will Morrissey <39009053+WillMo2198@users.noreply.github.com>
2021-06-07 15:03:17 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -29,24 +29,36 @@ def new_user(name):
     iteration3 = 0
     np.random.seed(1)
     in_data = []
-    for i in range(100):
-        _, img1 = cap.read()
-        gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-        faces1 = face_cascade.detectMultiScale(gray1, 1.3, 5)
-        for (x1, y1, w1, h1) in faces1:
-            roi1 = Image.fromarray(img1[y1:y1 + h1, x1:x1 + w1])
-            enhancer1 = ImageEnhance.Brightness(roi1)
-            roi1 = enhancer1.enhance(1.5)
-            roi1 = roi1.resize((75, 75))
-            roi1 = cv2.Canny(np.asarray(roi1), 100, 400)
-            cv2.imshow('img', img1)
-            Image.fromarray(roi1).save('Users/{0}/{1}.jpg'.format(name, i))
-            k1 = cv2.waitKey(30) & 0xff
-            if k1 == 27:
-                cap.release()
-                cv2.destroyAllWindows()
+    i = 0
+    if len(os.listdir('Users/{0}'.format(name))) < 3:
+        while len(os.listdir('Users/{0}'.format(name))) < 102:
+            _, img1 = cap.read()
+            gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+            faces1 = face_cascade.detectMultiScale(gray1, 1.3, 5)
+            if len(faces1) > 0:
+                for (x1, y1, w1, h1) in faces1:
+                    roi1 = Image.fromarray(img1[y1:y1 + h1, x1:x1 + w1])
+                    enhancer1 = ImageEnhance.Brightness(roi1)
+                    roi1 = enhancer1.enhance(1.5)
+                    roi1 = roi1.resize((75, 75))
+                    roi1 = cv2.Canny(np.asarray(roi1), 100, 400)
+                    cv2.imshow('img', img1)
+                    Image.fromarray(roi1).save('Users/{0}/{1}.jpg'.format(name, i))
+                    k1 = cv2.waitKey(30) & 0xff
+                    i += 1
+                    if k1 == 27:
+                        cap.release()
+                        cv2.destroyAllWindows()
+            else:
+                continue
+    else:
+        pass
     out = randint(0, 1)
-    for _ in trange(500):
+    for _ in trange(200):
+        if out == 0 and iteration2 == 100:
+            out = 1
+        if out == 1 and iteration3 == len(os.listdir('Users/{0}'.format(name))):
+            out = 0
         if out == 0:
             img1 = cv2.imread('Other/{0}'.format(os.listdir('Other')[iteration2]), 0)
             iteration2 += 1
@@ -60,17 +72,9 @@ def new_user(name):
                 roi_color1 = roi_color1.resize((75, 75))
                 roi_color1 = cv2.Canny(np.asarray(roi_color1), 100, 400)
                 in_data = np.asarray(Image.fromarray(roi_color1).resize((75, 75)))
-            if iteration2 == 399:
-                iteration2 -= 1
-                out = 1
-                continue
         else:
             in_data = cv2.imread('Users/{0}/{1}'.format(name, os.listdir('Users/{0}/'.format(name))[iteration3]), 0)
             iteration3 += 1
-            if iteration3 == 98:
-                iteration3 -= 1
-                out = 0
-                continue
         try:
             in_data = in_data.flatten()
             if not np.isnan(in_data[0]):
@@ -78,7 +82,6 @@ def new_user(name):
                     l0 = np.asarray([in_data])
                     l1 = nonlin(np.dot(l0, layer1))
                     l2 = nonlin(np.dot(l1, layer2))
-                    print(l2, out)
                     l2_error = out - l2
                     l2_delta = l2_error * nonlin(l2, deriv=True)
                     l1_error = l2_delta.dot(layer2.T)
@@ -88,13 +91,37 @@ def new_user(name):
         except AttributeError or ValueError:
             continue
         out = randint(0, 1)
+    np.save('Users/{0}/weights1.npy'.format(name), layer1)
+    np.save('Users/{0}/weights2.npy'.format(name), layer2)
+    contendors = os.listdir('Users')
+    contendors.remove(name)
+    print(contendors)
+    iteration = 0
+    if len(contendors) != 0:
+        for i in range(len(contendors)):
+            player_dir = os.listdir('Users/{0}'.format(name))
+            opp_dir = os.listdir('Users/{0}'.format(contendors[iteration]))
+            iteration2 = 0
+            iteration3 = 0
+            for _ in trange(int(len(player_dir)+len(opp_dir)/2)):
+                out = randint(0, 1)
+                if out == 0:
+                    in_data = cv2.imread('Users/{0}/{1}'.format(contendors[iteration], opp_dir[iteration2]), 0).flatten()
+                elif out == 1:
+                    in_data = cv2.imread('Users/{0}/{1}'.format(name, player_dir[iteration3]), 0).flatten()
+                for iiii in range(50):
+                    l0 = np.asarray([in_data])
+                    l1 = nonlin(np.dot(l0, layer1))
+                    l2 = nonlin(np.dot(l1, layer2))
+                    l2_error = out - l2
+                    l2_delta = l2_error * nonlin(l2, deriv=True)
+                    l1_error = l2_delta.dot(layer2.T)
+                    l1_delta = l1_error * nonlin(l1, deriv=True)
+                    layer2 += l1.T.dot(l2_delta)
+                    layer1 += l0.T.dot(l1_delta)
         np.save('Users/{0}/weights1.npy'.format(name), layer1)
         np.save('Users/{0}/weights2.npy'.format(name), layer2)
-    contendors = os.listdir('Users')
-    if len(contendors) > 1:
-        for i in range(len(contendors)):
-            if i+1 > len(contendors):
-                break
+        iteration += 1
 
 
 while True:
@@ -107,9 +134,8 @@ while True:
         roi = enhancer.enhance(1.5)
         roi = roi.resize((75, 75))
         roi = cv2.Canny(np.asarray(roi), 100, 400)
-        cv2.imshow('img', np.asarray(img))
         cv2.imshow('lines', np.asarray(roi))
-        roi = np.asarray(roi).flatten().flatten()
+        roi = np.asarray(roi).flatten()
         input_layer = np.asarray(roi)
         user_out = []
         out1 = []
@@ -119,14 +145,16 @@ while True:
                 hidden = nonlin(np.dot(input_layer, np.load('Users/{0}/weights1.npy'.format(ii))))
                 output = nonlin(np.dot(hidden, np.load('Users/{0}/weights2.npy'.format(ii))))
                 user_out.append([ii, output])
-            for ii in user_out:
-                user.append(ii[0])
-                out1.append(ii[1])
-                print(user[out1.index(max(out1))])
+                print(output, ii)
         else:
             hidden = nonlin(np.dot(input_layer, np.load('Users/{0}/weights1.npy'.format(os.listdir('Users')[0]))))
             output = nonlin(np.dot(hidden, np.load('Users/{0}/weights2.npy'.format(os.listdir('Users')[0]))))
-            print(output)
+            if output > 0.5:
+                img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                cv2.imshow('img', np.asarray(img))
+            elif output < 0.5:
+                img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                cv2.imshow('img', np.asarray(img))
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         cap.release()
