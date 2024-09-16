ba8f160b0314849e1b6665a72282abfd4a314de2
Will Morrissey <39009053+WillMo2198@users.noreply.github.com>
2024-09-16 14:39:20 -0500

    Update user.py

--- user.py
+++ user.py
@@ -3,6 +3,9 @@ from PIL import Image
 import numpy as np
 import cv2
 import os
+
+from cv2 import waitKey
+
 import ai123
 from time import sleep
 
@@ -43,18 +46,20 @@ class User:
         self.recognizer = Recognizer(name)
 
     def new_user(self):
-        if not os.path.exists(f'Users/{self.name}'):
+        if not os.path.exists(f'Users/{self.name}'):    # Make directory for new user training data
             os.makedirs(f'Users/{self.name}')
             sleep(1)
-        print('Press "a" to quit.')
+        print('Press "space" to quit.')
         while len(os.listdir(f'Users/{self.name}')) < 100:
-            screen, roi = self.recognizer.record()
+            try:
+                screen, roi = self.recognizer.record()
+            except TypeError:   # If no faces found, keep loop going
+                continue
             cv2.imshow('Scan', np.asarray(screen))
             cv2.imshow('ROI', np.asarray(roi))
             """Image.fromarray(np.array(roi)).save('Users/{0}/{1}.jpg'.format(self.name, photo_i))"""
-            if cv2.waitKey(33) == ord('a'):
-                quit()
+            cv2.waitKey(1)
 
 
 print('Initalizing...')
-User('Will').new_user()
+User('name').new_user()
